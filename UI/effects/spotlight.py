691115148301b28379dd9cd0c2bd3484cf06d0c8
Raltao Cai <104323079+doogyman@users.noreply.github.com>
2026-08-17 16:38:44 +0100

    v60

--- UI/effects/spotlight.py
+++ UI/effects/spotlight.py
@@ -10,26 +10,28 @@ class SpotLight(pygame.sprite.Sprite):
         self.rect = pygame.Rect(0, 0, 1, 1)
         self.rect.center = self.target_rect.center
         
-    
-        
-    
 class Ambience(pygame.sprite.Sprite):
     def __init__(self, radius, *groups):
         super().__init__(*groups)
         self.r = radius
         self.is_ambience = True
         self.pos = pygame.Vector2(0, 0)
-        self.image = pygame.Surface((Globals.SCREENWIDTH, Globals.SCREENHEIGHT), pygame.SRCALPHA)
+        self.image = pygame.Surface((1000, 1000), pygame.SRCALPHA)
         self.rect = self.image.get_frect(topleft = self.pos)
-        pygame.draw.rect(self.image, (38, 28, 38, 200), self.rect)
+        pygame.draw.rect(self.image, (38, 28, 38, 251), self.rect)
         self.reset()
         
     def reset(self):
         self.image.fill((38, 28, 38, 251))
     
     def cut_hole(self, target):
-        pygame.draw.circle(self.image, (0, 0, 0, 0), target, self.r)
+        for r in range(self.r, 0, -1):
+            alpha = int((1 - r/ self.r) * 251)
+            pygame.draw.circle(self.image, (0, 0, 0, alpha), target, self.r)
+            
         
     def update_cut_hole(self, target, dt=None):
         self.reset()
         self.cut_hole(target.center)
+        
+        
