6884299c6bd312d5111606ca2ed24c6272b0e3aa
Raltao Cai <104323079+doogyman@users.noreply.github.com>
2026-08-29 21:16:35 +0100

    v65

--- UI/font.py
+++ UI/font.py
@@ -6,9 +6,18 @@ class Font:
         self.charater_order = list('abcdefghijklmopqrstuwvxyz') + list('0123456789')
         self.spacing = 1
         self.font_sheet = pygame.image.load(sheet).convert_alpha()
-        self.font_info = pygame.surfarray.array2d(self.font_sheet)
-        self.charaters = {}
+        current_character_width = 0
+        self.characters = {}
+        counter = 0
         for x_coord in range(self.font_sheet.get_width()):
             color = self.font_sheet.get_at(x_coord, 0)
-            if color:
-                continue
+            if color[0] == 105:
+                char_img = clip(self.font_sheet, x_coord - current_character_width, 0, current_character_width, self.font_sheet.get_height())
+                self.characters[self.charater_order[counter]] = char_img.copy()
+                counter += 1
+                current_character_width = 0
+            else:
+                current_character_width += 1
+    
+    def draw(self, surf, text, loc):
+        pass
